07b16afd3721d8968d855773ad1a07c5bf0908f6
VijayPrabuR <vijairama577@gmail.com>
2021-05-17 19:54:54 +0530

    my project

---
+++ LayoutDemo/Views/DialpadPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutDemo
+{
+    
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class DialpadPage : ContentPage
+    {
+        public DialpadPage()
+        {
+            InitializeComponent();
+        }
+        private void Number_Entered(object sender, EventArgs e)
+        {
+            var enteredNumber = (sender as Button).Text;
+            this.phoneNumber.Text += enteredNumber;
+        }
+        private void Number_Deleted(object sender, EventArgs e)
+        {
+            this.phoneNumber.Text = null;
+        }
+    }
+}
---
+++ LayoutDemo/Views/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        async void gotoDialpadPage(object sender, EventArgs e)
+        {
+            string user = username.Text;
+            string pass = password.Text;
+            if(String.IsNullOrEmpty(user) || String.IsNullOrEmpty(pass))
+            {
+                await DisplayAlert("Failure msg", "Enter credentials", "OK");
+            }
+            else
+            {
+                await Navigation.PushAsync(new DialpadPage());
+            }
+            
+        }
+    }
+}
